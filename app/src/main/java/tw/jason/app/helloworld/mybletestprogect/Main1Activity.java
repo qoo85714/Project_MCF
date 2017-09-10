336358ce22d0c395911897a0a7146bc53dd9bbed
Coral <coral00685@gmail.com>
2017-09-10 16:38:46 +0800

    20170910

---
+++ app/src/main/java/tw/jason/app/helloworld/mybletestprogect/Main1Activity.java
@@ -0,0 +1,16 @@
+package tw.jason.app.helloworld.mybletestprogect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class Main1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+
+    }
+}
